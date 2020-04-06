2b70d306bea58eb8d9aa8314579aae5efe451e92
Kiarash Banihashem <k.banihashem73@gmail.com>
2020-04-06 13:18:12 +0430

    Added login page (still working on it though)

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from users import views
 
@@ -7,5 +8,6 @@ app_name="users"
 urlpatterns = [
     path('', views.user_home),
     path(r'register/', views.user_register, name="register"),
-    path(r'login/', views.user_login, name="login")
+    path(r'login/', auth_views.LoginView.as_view(template_name='home/login.html'), name="login"),
+    path(r'logout/', auth_views.LogoutView.as_view(template_name='home/logout.html'), name="logout"),
 ]
