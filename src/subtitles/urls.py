45d0a970e2b6862c12d0ebb94fff31d724b5aab6
Aryo Lotfi <aryolotfi77@gmail.com>
2020-04-25 11:01:22 +0430

    Changed subtitle downloading system. Now users should initiate the download themselves. They can also download new words.

--- src/subtitles/urls.py
+++ src/subtitles/urls.py
@@ -6,4 +6,6 @@ app_name="subtitles"
 
 urlpatterns = [
     path('', views.sub_processor, name='sub_processor'),
+    path('download_sub', views.download_sub, name='download_sub'),
+    path('download_words_list', views.download_words_list, name='download_words_list'),
 ]
--- src/subtitles/views.py
+++ src/subtitles/views.py
@@ -3,26 +3,42 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from subtitles.services import process_sub
 from mimetypes import guess_type
+
+
+
 @login_required
 def sub_processor(request):
     if request.method == 'POST' and ('subtitle' in request.FILES):
         uploaded_sub = request.FILES['subtitle']
         name = uploaded_sub.name
-        type = name[name.rfind('.'):]
+        file_type = name[name.rfind('.'):]
         text = uploaded_sub.read().decode('iso-8859-1')
-        result = process_sub(text, type, 3)
+        result = process_sub(text, file_type, 3)
+        new_words = []
         for word in result.keys():
             replacement = "{0} ({1})".format(word, result[word][0])
             text = text.replace(word, replacement)
-            print(replacement)
-        filename = name[:name.rfind('.')] + "enchanted" + type
-        content = text.encode('utf-8')
-        response = HttpResponse(content, content_type=guess_type(filename))
-        response['Content-Disposition'] = 'attachment; filename={0}'.format(filename)
-        return response
-
-
-        # print(text)
+            new_words.append(replacement)
+        request.session['filename'] = name[:name.rfind('.')] + "_enchanted" + file_type
+        request.session['content'] = text
+        request.session['new_words'] = new_words
+        return render(request, 'subtitles/succeed.html', context={'count': len(new_words)})
 
     return render(request, 'subtitles/upload.html')
 
+
+def download_sub(request):
+    filename = request.session['filename']
+    content = request.session['content'].encode('utf-8')
+    response = HttpResponse(content, content_type=guess_type(filename))
+    response['Content-Disposition'] = 'attachment; filename={0}'.format(filename)
+    return response
+
+
+def download_words_list(request):
+    filename = "New words.txt"
+    content = "\n".join(request.session['new_words'])
+    content.encode("utf-8")
+    response = HttpResponse(content, content_type=guess_type(filename))
+    response['Content-Disposition'] = 'attachment; filename={0}'.format(filename)
+    return response
